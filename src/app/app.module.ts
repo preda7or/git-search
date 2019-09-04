bf6ad49749fd31e3cd459ff5014eeb08289d5a50
Gabor Stefan <gabor.stefan@turicode.com>
2019-09-05 00:03:37 +0200

    Angular version bump Self contain (fonts, icons) Styles move into folder Strick null check enabled Favicon added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { MatAutocompleteModule, MatButtonModule, MatIconModule, MatTableModule, MatToolbarModule } from "@angular/material";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TopNavComponent } from "./components/top-nav/top-nav.component";
+
+const materialModules = [MatToolbarModule, MatIconModule, MatButtonModule, MatAutocompleteModule, MatTableModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ...materialModules],
+  declarations: [AppComponent, TopNavComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/components/top-nav/top-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-top-nav',
+  templateUrl: './top-nav.component.html',
+  styleUrls: ['./top-nav.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TopNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
